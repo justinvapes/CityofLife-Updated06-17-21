cc79d9b748c46479681444d609de0945f330cdfb
justinvapes <86035751+justinvapes@users.noreply.github.com>
2021-06-17 21:24:54 -0700

    Add files via upload

---
+++ mloloaderv2/fxmanifest.lua
@@ -0,0 +1,18 @@
+description 'Constans multiple mlos as a single resource'
+name 'Bulk mlo loader'
+
+fx_version 'adamant'
+games {'gta5'}
+
+
+file 'v_kitch.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/int_corporate.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/v_kitch.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/gabz_diner_props.ytyp'
+data_file 'TIMECYCLEMOD_FILE' 'bs_timecycmod.xml'
+data_file 'SCALEFORM_DLC_FILE' 'stream/int1756029552.gfx'
+
+this_is_a_map 'yes'
+client_script '65248.lua'
+client_script '43212.lua'
+client_script '94563.lua'
